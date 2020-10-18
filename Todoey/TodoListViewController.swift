021696e6dfd385c1e5923657134827af6d509cf8
MariaYelfimova <maria.yelfimova@gmail.com>
2020-10-18 08:54:33 +0300

    add new item functionality added

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,12 +9,67 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
+    
+    var items = ["Smile at Denis","Find a perfect screen","Buy carrot"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //MARK - TableView Datasource methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+                
+        let cell = tableView.cellForRow(at: indexPath)
+        if cell?.accessoryType == .checkmark {
+            cell?.accessoryType = .none
+        } else { cell?.accessoryType = .checkmark }
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+    
+    //MARK - Add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        //creating a local variable, so that to store here the refference to the alertText field
+        var textField = UITextField()
+        
+        // Here I define a VC that will execute my alert.
+        let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
+        
+        // Here I define my alert action - this is what should happend once the user clicks
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            // what will happen once the user clicks on that button
+            print("Success!")
+            self.items.append(textField.text ?? "New Item")
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            //print(alertTextField)
+            textField = alertTextField
+            print(alertTextField.text)
+            print("Now")
+        }
+        //Here I call the action
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
 
